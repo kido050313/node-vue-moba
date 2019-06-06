2700febb481748611ef69e657a10755e50fda066
lmy <1059064141@qq.com>
2019-06-06 16:19:31 +0800

    删除

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -7,7 +7,12 @@
       <el-table-column fixed="right" label="操作" width="150">
         <template slot-scope="scope">
           <!-- <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button> -->
-          <el-button type="text" size="small" @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click="$router.push(`/categories/edit/${scope.row._id}`)"
+          >编辑</el-button>
+          <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -24,6 +29,27 @@ export default {
     async fetch() {
       const res = await this.$http.get("categories");
       this.items = res.data;
+    },
+    remove(row) {
+      this.$confirm(`是否确定删除分类 "${row.name}"`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(async () => {
+        const res = await this.$http.delete(`categories/${row._id}`);
+        if (res.data.success) {
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+          this.fetch()
+        } else {
+          this.$message({
+            type: "faild",
+            message: "删除失败!"
+          });
+        }
+      });
     }
   },
   created() {
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -2,11 +2,13 @@ module.exports = app => {
   const express = require('express')
   const router = express.Router()
   const Category = require('../../models/Category')
+
   // 新建分类
   router.post('/categories', async (req, res) => {
     const model = await Category.create(req.body)
     res.send(model)
   })
+
   // 更新
   router.put('/categories/:id', async (req, res) => {
     const model = await Category.findByIdAndUpdate(req.params.id, req.body)
@@ -24,6 +26,15 @@ module.exports = app => {
     const model = await Category.findById(req.params.id)
     res.send(model)
   })
+
+  // 删除分类
+  router.delete('/categories/:id', async (req, res) => {
+    await Category.findByIdAndDelete(req.params.id, req.body)
+    res.send({
+      success: true
+    })
+  })
+
 
   app.use('/admin/api', router)
 }
