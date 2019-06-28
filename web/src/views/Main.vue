8939537b91a6af0dcc770b660560026a3febdd23
lmy <1059064141@qq.com>
2019-06-28 18:19:51 +0800

    添加首页布局

---
+++ web/src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+      <img src="../assets/images/logo.png" height="30">
+      <div class="px-3">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-gray-1 fs-xxs">团队成就更多</div>
+      </div>
+    </div>
+    <div class="nav">
+      nav
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default({
+
+})
+</script>
